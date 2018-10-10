dd947a650ea117beab5aa728e7c8f31858e6d2c0
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-10 16:27:33 -0600

    Sven's Persona and User Story index.php

--- index.php
+++ index.php
@@ -1,7 +1,47 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: overlord
- * Date: 10/10/18
- * Time: 11:53 AM
- */
+<!DOCTYPE html>
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+	</head>
+	<body>
+		<h1>Data Design Project</h1>
+		<h2>User Persona:</h2>
+		<img src="Sven.jpg" alt="Sven Thorsfriend" height="500">
+		<ul>
+			<li><strong>Name:</strong> Sven Thorsfriend</li>
+			<li><strong>Age:</strong> 35</li>
+			<li><strong>Gender:</strong> Male</li>
+			<li><strong>Location:</strong> Savannah, Georgia</li>
+			<li><strong>Profession:</strong> Corporate Lawyer</li>
+			<li><strong>Relationship Status:</strong> Married, No Children</li>
+			<li><strong>Hobbies:</strong> Restoring Classic German Sports Cars, Autocross, Photography</li>
+			<li><strong>Likes: </strong> Clean design and easily accessible and sharable information.</li>
+			<li><strong>Owned Technology:</strong></li>
+			<ul>
+				<li><strong>Phone:</strong> One+ 6 running Oreo based OxygenOS</li>
+				<li><strong>Laptop:</strong> 15" MacbookPro early 2017 OS X 10.14</li>
+				<li><strong>Smart Watch:</strong> Apple Watch 4</li>
+				<li><strong>Camera:</strong> Nikon D850 DLSR, Light L16</li>
+			</ul>
+			<li><strong>Technology Comfort Level:</strong></li>
+			<ul>
+				<li>Very comfortable with technology and active daily on social
+					media platforms (facebook, instagram, pintrest)</li>
+			</ul>
+			<li><strong>Technological Needs:</strong></li>
+			<ul>
+				<li>A robust comment system</li>
+				<li>Share linking to social media</li>
+				<li>Easy access to the work of favorite writers</li>
+			</ul>
+			<li><strong>Frustrations:</strong></li>
+			<ul>
+				<li>Sifting through multiple pages to find previously posted articles</li>
+				<li>Required waiting periods before comments become public</li>
+				<li>Bad article suggestions on differing interest categories</li>
+			</ul>
+		</ul>
+		<h2>User Story:</h2>
+		<p>Sven wants to be informed and entertained with all things automobile related in his free time with the option to
+			interact with fellow readers.</p>
+	</body>
