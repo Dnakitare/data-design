635758d9a9eb8b308db33438de90bba1570865e2
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-10 15:29:37 -0600

    Sven's Persona

--- use-case.php
+++ use-case.php
@@ -1,7 +1,15 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: overlord
- * Date: 10/10/18
- * Time: 11:53 AM
+/*
+ * Use Case:
+ *
+ * Sven wants to be able to visit a website between client meetings that will provide great information on the state of
+ * the automotive industry while also providing extremely entertaining articles on all things cars. Because he not only
+ * tracks his cars on the weekends but also rebuilds old cars in his garage, he would like to be able to interact with
+ * the authors and other readers of his favorite articles. He would also like to share articles he finds
+ * interesting or amusing with his social network easily.
+ *
+ * User Story:
+ *
+ * Sven wants to be informed and entertained with all thins automobile related in his free time with the option to
+ * interact with fellow readers.
  */
