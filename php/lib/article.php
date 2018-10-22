585b780e3e41d7a76c0d8653269e4754281864a7
Daniel Nakitare <dnakitare@cnm.edu>
2018-10-21 20:29:18 -0600

    boss fight attempt 1

--- php/lib/article.php
+++ php/lib/article.php
@@ -1,7 +1,8 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: overlord
- * Date: 10/17/18
- * Time: 1:58 PM
- */
+namespace Dnakitare\DataDesign;
+
+// instantiate
+$article = new Article();
+$article->setArticleId("f91c414d7e2b4184b15ee1a6fc266b28");
+$article->setArticleContent("this is a new article");
+
